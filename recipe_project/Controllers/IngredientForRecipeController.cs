9fc572bf8027130f9e2896ed69e242b4754eae25
TehilaGuetta <g0583202927@gmail.com>
2025-01-29 12:17:41 +0200

    Add files via upload

---
+++ recipe_project/Controllers/IngredientForRecipeController.cs
@@ -0,0 +1,39 @@
+﻿using BLL.interfaces;
+using DAL.models;
+using DTO.respository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace recipe_project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientForRecipeController : ControllerBase
+    {
+        IingredientForRecipeBLL _IingredientForRecipeBLL;
+        public IngredientForRecipeController(IingredientForRecipeBLL iingredientForRecipeBLL)
+        {
+            _IingredientForRecipeBLL = iingredientForRecipeBLL;
+        }
+        [HttpGet("getall")]
+        public ActionResult<List<IngredientForRecipe>> getall()
+        {
+            return Ok(_IingredientForRecipeBLL.getall());
+        }
+        [HttpGet("getingredientbycode/{code}")]
+        public ActionResult<List<IngredientForRecipe>> getingredientbycode(int code)
+        {
+            return Ok(_IingredientForRecipeBLL.getingredientbycode(code));
+        }
+        [HttpPost("addlistingredient")]
+        public ActionResult <bool> addlistingredient(List<IngredientForRecipeDTO> list)
+        {
+            return Ok(_IingredientForRecipeBLL.addlistingredient(list));
+        }
+        [HttpPost("add")]
+        public ActionResult<bool> add(IngredientForRecipeDTO s)
+        {
+            return Ok(_IingredientForRecipeBLL.add(s));
+        }
+    }
+}
---
+++ recipe_project/Controllers/recipeTableController.cs
@@ -0,0 +1,35 @@
+﻿using BLL.interfaces;
+using DAL.models;
+using DTO.respository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace recipe_project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class recipeTableController : ControllerBase
+    {
+        IrecipeTableBLL _IrecipeTableBLL;
+        public recipeTableController(IrecipeTableBLL irecipeTableBLL)
+        {
+            _IrecipeTableBLL = irecipeTableBLL;
+        }
+        [HttpGet("getall")]
+        public ActionResult<List<RecipeTableDTO>> getall()
+        {
+            return Ok(_IrecipeTableBLL.getall());
+        }
+        [HttpPost("add")]
+        public ActionResult<int> addrecipe(RecipeTableDTO recipeTableDTO)
+        {
+            return Ok(_IrecipeTableBLL.add(recipeTableDTO));
+        }
+        [HttpGet("getbycode/{id}")]
+        public ActionResult<RecipeTableDTO> getbyuser(int id)
+        {
+            return Ok(_IrecipeTableBLL.getbyusercode(id));
+        }
+            
+    }
+}
